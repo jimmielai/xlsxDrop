b6f0ffcb808b9f240dc55da466b7b9775b121db8
Kevin Splittgerber <grbspltt@gmail.com>
2015-09-11 09:28:32 -0700

    express server started

--- server/src/app.js
+++ server/src/app.js
@@ -1,2 +1,13 @@
 import express from 'express'
 let app = express();
+
+app.get('/',(req,res,next)=>{
+  res.send('hai!')
+})
+
+let server = app.listen(8081,()=>{
+  let host = server.address().address,
+    port = server.address().port;
+
+  console.log('listening on http://%s:%s', host, port)
+})
