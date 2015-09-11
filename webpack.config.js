803874d9ed884f87b2a20335feb87a4810cc4aa3
Kevin Splittgerber <grbspltt@gmail.com>
2015-09-11 09:45:53 -0700

    webpack vendor assets set, bower config started

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,19 @@
+var path = require('path'),
+  ROOT_PATH = __dirname;
+
 module.exports = {
   entry: './client/src/main.js',
-
+  output:{
+    filename: './client/build/js/bundle.[hash].js',
+    publicPath: './client/build/assets'
+  },
+  resolve:{
+    extensions:['','.js','.jsx'],
+    path: ROOT_PATH
+  },
+  module:{
+    loaders:[
+      {test:/\.jsx$/, include:path.resolve(ROOT_PATH,'client/src'), loader:'babel-loader'}
+    ]
+  }
 }
