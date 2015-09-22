e1595cc78b061e2353a7cd78e6af2c562ec4bf2d
Kevin <grbspltt@gmail.com>
2015-09-22 10:25:04 -0700

    started bulk insert to collection

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,12 +1,18 @@
 import express from 'express'
 let router = express.Router();
 
-router.get('/',(req,res,next)=>{
+router.get('/', (req, res, next)=>{
   return res.json('aloha')
 });
-router.post('/data', (req,res,next)=>{
-  console.log(req.body);
-  res.json({success:true});
+router.post('/data', (req, res, next)=>{
+  let body = req.body || [];
+  if(body.length){
+    req.models.acp.collection.insertAsync(body).then(function(v){
+      //todo returns bulk write result if writing an array of docs
+    })
+  } else {
+    return res.json({success: false});
+  }
 });
 
 export default router;
