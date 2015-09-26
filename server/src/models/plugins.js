a2c3e0703f7b10fee18412788687e9e7354e3ee8
Kevin <grbspltt@gmail.com>
2015-09-26 12:25:30 -0700

    starting schema constructor function

--- server/src/models/plugins.js
+++ server/src/models/plugins.js
@@ -1,5 +1,15 @@
-
+import { Schema } from 'mongoose'
+import ks from 'ks'
 export const shortDate = function shortDate(val){
-    if(!val) return val;
-    return (val.getMonth() + 1) + "/" + val.getDate() + "/" + val.getFullYear();
+  if(!val) return val;
+  return (val.getMonth() + 1) + "/" + val.getDate() + "/" + val.getFullYear();
+}
+
+export const createSchema = (schemaName=ks.randomPassword(36),options)=>{
+  let schema = new Schema()
+
+
+  return {
+    schemaName: schemaName
+  }
 }
